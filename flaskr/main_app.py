c7b9ed4fa222438e98877474337b5d22dba6f3a5
Stephen West <stevewestinlondon@gmail.com>
2023-05-16 12:22:03 +0100

    added about page added move html tags

--- flaskr/main_app.py
+++ flaskr/main_app.py
@@ -4,12 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def get_home_page():
-    return "<h1>Hello Flask Example</h1>"
+    return """<h1>The Football Supporter Site - Home Page</h1>
+    <p>Welcome to the football supporter site</p>
+    <p><a href='/index'>Home</a> | <a href='/about'>About</a></p>"""
+
+@app.route('/about')
+def get_about_page():
+    return """<h1>The Football Supporter Site - About Page</h1>
+    <p>This flask example will create a football supporter site</p>
+    <p><a href='/index'>Home</a> | <a href='/about'>About</a></p>"""
 
 
 if __name__ == "__main__":
-    app.run()
+    # app.run()
 
     # Optional, host address often required if running on a VM
-    # app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
