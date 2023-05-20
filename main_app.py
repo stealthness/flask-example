c084e8589f01a8d042176924bb0a4a298666a129
Stephen West <stevewestinlondon@gmail.com>
2023-05-20 22:46:54 +0100

    moved main html pages into subdirectory

--- main_app.py
+++ main_app.py
@@ -13,6 +13,7 @@ data = SimpleDataManager()
 # data.load_users(os.path.join('res', 'user_data.csv'))
 data.load_user_from_db(csv_users_filename, db_filename)
 
+
 @app.route('/', methods=(['POST', 'GET']))
 @app.route('/index', methods=(['POST', 'GET']))
 def get_home_page():
@@ -22,7 +23,8 @@ def get_home_page():
             return redirect(url_for('get_user_page', username=user.lower()))
         else:
             return redirect(url_for('get_new_user_page'))
-    return render_template('index.html', title='Home Page')
+    return render_template(os.path.join('pages', 'index.html'), title='Home Page')
+
 
 @app.route('/about')
 def get_about_page():
@@ -31,12 +33,12 @@ def get_about_page():
         shuffle(data.users)
         users = data.users[:3]
 
-    return render_template('about.html', title='Home Page', users=users)
+    return render_template(os.path.join('pages', 'about.html'), title='Home Page', users=users)
 
 
 @app.route('/warn')
 def get_warning_page(message=''):
-    return render_template('error_page.html', title='Error Page', message=message)
+    return render_template(os.path.join('pages', 'error_page.html'), title='Error Page', message=message)
 
 
 @app.route('/user/<string:username>')
@@ -47,12 +49,12 @@ def get_user_page(username):
     if not safe_username.title() in data.users:
         return redirect(url_for('get_warning_page'))
 
-    return render_template('user_page.html', title='User Page' ,username=safe_username)
+    return render_template(os.path.join('pages', 'user_page.html'), title='User Page' ,username=safe_username)
 
 
 @app.route('/all_users')
 def get_all_users_page():
-    return render_template("all_users.html", title="All Users Page", users=data.users)
+    return render_template(os.path.join('pages', "all_users.html"), title="All Users Page", users=data.users)
 
 
 @app.route('/new_user', methods = ([ 'POST','GET']))
@@ -62,12 +64,12 @@ def get_new_user_page():
         new_location = escape(request.form['location']).title()
         new_club = escape(request.form['club']).title()
         if new_user in data.users or new_user == '':
-            return render_template('add_new_user_page.html')
+            return render_template(os.path.join('pages', 'add_new_user_page.html'))
         data.users.append(new_user)
         conn = data.get_db_connection(db_filename)
         conn.execute(f'insert into users values("{new_user}", "{new_location}", "{new_club}");')
         return redirect(url_for ('get_user_page', username=new_user.lower()))
-    return render_template('add_new_user_page.html', title="New User Page")
+    return render_template(os.path.join('pages', 'add_new_user_page.html'), title="New User Page")
 
 if __name__ == "__main__":
     # app.run()
